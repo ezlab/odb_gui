55b5673704f56f347a41696b782365665534199e
kamushkin <kamushkin@users.noreply.github.com>
2015-09-19 18:22:05 +0200

    add files uploading

--- static/js/navigation.js
+++ static/js/navigation.js
@@ -51,6 +51,9 @@
 		else if (params.page){
 			app.loadPage('static/pages/' + params.page + '.html');
 		}
+		else if (params.files){
+			app.showFiles();
+		}
 		else if (params.back){
 			return history.go(-1);
 		}
