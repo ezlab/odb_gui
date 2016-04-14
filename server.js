ea8aaf976dcba589caede61f9ddaac62372d1ddc
kamushkin <kamushkin@users.noreply.github.com>
2016-04-14 20:34:35 +0200

    add ogdetails proxy

--- server.js
+++ server.js
@@ -23,7 +23,7 @@ cfg.proxy = {
 	}
 };
 
-var	routes = ['/group', '/search', '/blast', '/orthologs', '/siblings', '/fasta', '/tab', '/tree'];
+var	routes = ['/group', '/search', '/blast', '/orthologs', '/ogdetails', '/siblings', '/fasta', '/tab', '/tree'];
 
 
 var app = express.Router();
